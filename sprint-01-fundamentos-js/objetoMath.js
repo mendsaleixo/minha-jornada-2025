d2efa13802f652ce094b957d61046cff5fb0ba42
“MendsAleixo” <mendelson.aleixo@gmail.com>
2025-05-28 05:20:00 -0300

    feat: pratica conceitos de Numbers e Math

---
+++ sprint-01-fundamentos-js/objetoMath.js
@@ -0,0 +1,11 @@
+let num1 = 9.49578;
+//let num2 = Math.floor(num1); //Arredonda para baixo.
+//let num2 = Math.ceil(num1) //Arredonda para cima
+//let num2 = Math.round(num1);// Arredonda para o mais próximo
+//console.log(Math.max(1,2,3,4,34,56,32,16,1500,-2,-68,39)); //Retorna o maior numero
+//console.log(Math.min(1,2,3,4,34,56,32,16,1500,-2,-68,39)); //Retorna o menor numero
+//console.log(Math.random()); //Gera números aleatórios entre 0 e 1.
+//const aleatorio = Math.random() * (10 - 5) + 5;//Dá para criar expressões.
+//const zeroDez = Math.floor(Math.random() * 11)// sempre escolher de 0 a dez inteiro
+//console.log(Math.pow(2,10));//Potenciação (Mais fácil usar o operador que o objeto math); 
+
