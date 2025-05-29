90b8ac0f0ec6ec597d3a006c223cf1286e7215a6
“MendsAleixo” <mendelson.aleixo@gmail.com>
2025-05-29 17:15:47 -0300

    fix: corrige cálculo de IMC no exercício

--- sprint-01-fundamentos-js/ex-imc/script.js
+++ sprint-01-fundamentos-js/ex-imc/script.js
@@ -1,9 +1,8 @@
-//função para receber
+const name = document.getElementById("name").value;
+const lastname = document.getElementById("lastname").value;
+const weight = Number(document.getElementById("weight").value);
+const height = Number(document.getElementById("height").value);
 
-const name = "mends"; //document.getElementById("name");
-const lastname = "Aleixo"; //document.getElementById("lastname");
-const weight = 78; //Number(document.getElementById("weight".value));
-const height = 1.67; //Number(document.getElementById("height".value));
 const calc = (a, b) => a / (b * b);
-const result = calc(weight, height);
-console.log(result.toFixed(2));
+const IMC = calc(weight, height);
+const result = IMC.toFixed(2);
