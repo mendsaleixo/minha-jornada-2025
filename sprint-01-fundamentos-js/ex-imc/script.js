f746ee4aa1075c721e93c63096660369050dce71
“MendsAleixo” <mendelson.aleixo@gmail.com>
2025-05-29 17:01:12 -0300

    feat: implementa logica J Sno exercício

--- sprint-01-fundamentos-js/ex-imc/script.js
+++ sprint-01-fundamentos-js/ex-imc/script.js
@@ -1,1 +1,11 @@
 //função para receber
+
+const name = document.getElementById(name);
+const lastname = "lAleixo";
+const weight = 78;
+const height = 1.67;
+const result = function (a, b) {
+  return a / (b * b);
+};
+const calc = result(weight, height);
+console.log(calc.toFixed());
