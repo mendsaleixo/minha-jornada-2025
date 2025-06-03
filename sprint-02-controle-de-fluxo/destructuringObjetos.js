a94920d042315b11af0f6ee3d24b2dbcab8e065b
“MendsAleixo” <mendelson.aleixo@gmail.com>
2025-06-03 05:54:11 -0300

    feat: pratica com destructuring em objecos.

---
+++ sprint-02-controle-de-fluxo/destructuringObjetos.js
@@ -0,0 +1,20 @@
+const pessoa = {
+  nome: "mends",
+  sobrenome: "Aleixo",
+  idade: 30,
+  endereco: {
+    rua: "Av Brasil",
+    numero: 320,
+  },
+};
+//const nome = pessoa.nome; //atribuição convencional
+//const {nome} = pessoa //atribuição por desestruturação.
+const { nome, sobrenome, idade, carro = "sandero" } = pessoa;
+
+console.log(nome, sobrenome, idade);
+console.log(pessoa);
+console.log(carro); //foi criada um const carros no mesmo momento d aatribuição.
+
+const { name, ...resto } = pessoa; //Usando parâmento rest
+console.log(resto)
+
