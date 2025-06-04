88c6a51e41c4ed20dbbffd8262a274117001e414
“MendsAleixo” <mendelson.aleixo@gmail.com>
2025-06-04 15:30:28 -0300

    feat:praticando while e do while.

--- sprint-02-controle-de-fluxo/estruturasRepeticao.js
+++ sprint-02-controle-de-fluxo/estruturasRepeticao.js
@@ -26,7 +26,40 @@
 
 /**For of */
 //Retorna valores
-const nome = "Mendelson Aleixo";
-for (let valor of nome) {
-  console.log(valor);
-} //retorna o valor
+// const nome = "Mendelson Aleixo";
+// for (let valor of nome) {
+//   console.log(valor);
+// } //retorna o valor
+
+/**While e Do While*/
+//Enquanto a condição for verdadeira, faça
+// let i = 0;
+
+// while (i <= 10) {
+//   console.log(i)
+//   i++
+// }
+
+//número aleatório
+function random(min, max) {
+  const r = Math.random() * (max - min) + min;
+  return Math.floor(r); //arredonda pra baixo com o floor
+}
+
+const min = 1;
+const max = 50;
+let rand = random(min, max);
+
+while (rand !== 10) {
+  rand = random(min, max);
+  console.log(rand);
+}
+
+/**Do While */
+//faça primeiro algo e depois verifique a condição
+do {
+  rand = random(min, max);
+  console.log(rand);
+} while (rand !== 10);
+
+/**Break Continue*/
