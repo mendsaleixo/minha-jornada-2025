f32d1daa2f953b503577a701429cb96558c7100b
“MendsAleixo” <mendelson.aleixo@gmail.com>
2025-06-13 19:21:21 -0300

    Feat: funções retorno.

--- sprint-04-funcoes-avancado/retornoFnc.js
+++ sprint-04-funcoes-avancado/retornoFnc.js
@@ -27,3 +27,16 @@
 // };
 // console.log(p1, p2);
 
+function criaMultiplicador(multiplicador) {
+  return function (n) {
+    return n * multiplicador;
+  };
+}
+
+const duplica = criaMultiplicador(2);
+const triplica = criaMultiplicador(3);
+const quadriplica = criaMultiplicador(4);
+
+console.log(duplica(2));
+console.log(triplica(2));
+console.log(quadruplica(2));
