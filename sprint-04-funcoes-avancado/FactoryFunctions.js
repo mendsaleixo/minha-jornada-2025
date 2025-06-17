1bb4bf937adb1cc7207eecad516f04c7a3abe53a
“MendsAleixo” <mendelson.aleixo@gmail.com>
2025-06-17 18:32:11 -0300

    Feat: praticando Factory - usandor setter.

--- sprint-04-funcoes-avancado/FactoryFunctions.js
+++ sprint-04-funcoes-avancado/FactoryFunctions.js
@@ -17,7 +17,20 @@ function criaPessoa(nome, sobrenome, peso, altura) {
   return {
     nome,
     sobrenome,
-    falaAlgo(mensagem) {
+    //Getter
+    get nomeCompleto() {
+      return `${this.nome} ${this.sobrenome}`;
+    },
+    //Setter - Permite receber valores e setar dentro da função/atributo nomeCompleto com valores que vem de fora da factory.
+    set nomeCompleto(valor) {
+      valor = valor.split(" "); //transforma o valor recebido pelo setter num array.
+      this.nome = valor.shift(); //pega o primeiro item do array e joga no atributo nome.
+      this.sobrenome = valor.join(" "); //pega os demais itens do array e joga no atributo sobrenome
+      console.log(valor);
+    },
+
+    falaAlgo(mensagem = "olá") {
+      //olá é valor padrão
       return `${this.nome} ${this.sobrenome} está dizendo ${mensagem}`;
     },
     peso,
@@ -30,7 +43,22 @@ function criaPessoa(nome, sobrenome, peso, altura) {
   };
 }
 
-const p1 = criaPessoa("mends", "aleixo", 76, 1.67);
+
 // console.log( p1.imc());//sem getter é uma função
-console.log(p1.imc)//está sem () pq é entendido como atributo devido ao get. 
-console.log(p1.falaAlgo("olá, mundo!"));
+//console.log(p1.imc); //está sem () pq é entendido como atributo devido ao get.
+//console.log(p1.falaAlgo("olá, mundo!"));
+//p1.nomeCompleto = "Luana Jaime Tocchio"; //manda essa valor pro setter e retorna pelo console.log de lá.
+// console.log(p1.nomeCompleto);
+// console.log(p1.nome);
+// console.log(p1.sobrenome);
+// console.log(p1.falaAlgo());
+
+const p1 = criaPessoa("Mends", "Aleixo", 76, 1.67);
+const p2 = criaPessoa("Luana", "Tocchio", 64, 1.67);
+const p3 = criaPessoa("Caio", "Tocchio", 35, 1.28);
+const p4 = criaPessoa("mends", "aleixo", 28, 1.18);
+
+console.log(p1.imc)
+console.log(p2.imc)
+console.log(p3.imc)
+console.log(p4.imc)
