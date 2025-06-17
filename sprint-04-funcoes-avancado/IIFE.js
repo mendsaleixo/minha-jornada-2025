1e3578ef128e31962a718cd1b161f0590743dd84
“MendsAleixo” <mendelson.aleixo@gmail.com>
2025-06-17 05:32:43 -0300

    Feat: praticando IIFE - Immediately invoked function expression.

---
+++ sprint-04-funcoes-avancado/IIFE.js
@@ -0,0 +1,9 @@
+// IIFE - Immediately invoked function expression
+//Criada para não sair para o escopo global
+
+(function () {
+  const nome = "Mends";
+  console.log(`Olá, ${nome}`);
+})();
+
+const nome = "Aleixo";
