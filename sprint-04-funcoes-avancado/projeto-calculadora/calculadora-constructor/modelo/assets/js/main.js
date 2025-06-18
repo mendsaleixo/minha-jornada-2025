ced6fd0508cf7a3b53ce42d4d4d8fb1be18ae01d
“MendsAleixo” <mendelson.aleixo@gmail.com>
2025-06-18 05:08:01 -0300

    Feat: Calculador com função construtora.

--- sprint-04-funcoes-avancado/projeto-calculadora/calculadora-constructor/modelo/assets/js/main.js
+++ sprint-04-funcoes-avancado/projeto-calculadora/calculadora-constructor/modelo/assets/js/main.js
@@ -1,85 +1,82 @@
-function criaCalculadora() {
-  return {
-    display: document.querySelector(".display"),
-    btnClear: document.querySelector(".btn-clear"),
-
-    inicia() {
+function Calculadora() {
+  (this.display = document.querySelector(".display")),
+    (this.btnClear = document.querySelector(".btn-clear")),
+    (this.inicia = function () {
       this.cliqueBotoes(); //sempre que se referir a uma chave que está dentro do objeto, tem que usar o this.
       this.pressionaBackSpace();
       this.pressionaEnter();
-    },
+    });
 
-    pressionaBackSpace() {
-      this.display.addEventListener("keydown", (e) => {
-        if (e.keyCode === 8) {
-          e.preventDefault();
-          this.clearDisplay();
-        }
-      });
-    },
+  this.pressionaBackSpace = function () {
+    this.display.addEventListener("keydown", (e) => {
+      if (e.keyCode === 8) {
+        e.preventDefault();
+        this.clearDisplay();
+      }
+    });
+  };
 
-    pressionaEnter() {
-      this.display.addEventListener("keyup", (e) => {
-        if (e.keyCode === 13) {
-          this.realizaConta();
-        }
-      });
-    },
+  this.pressionaEnter = function () {
+    this.display.addEventListener("keyup", (e) => {
+      if (e.keyCode === 13) {
+        this.realizaConta();
+      }
+    });
+  };
 
-    realizaConta() {
-      let conta = this.display.value;
+  this.realizaConta = function () {
+    let conta = this.display.value;
 
-      try {
-        conta = eval(conta);
+    try {
+      conta = eval(conta);
 
-        if (!conta) {
-          alert("Conta inválida");
-          return;
-        }
-
-        this.display.value = String(conta);
-      } catch (e) {
+      if (!conta) {
         alert("Conta inválida");
         return;
       }
-    },
 
-    clearDisplay() {
-      this.display.value = "";
-    },
-
-    apagaUm() {
-      this.display.value = this.display.value.slice(0, -1);
-    },
-
-    cliqueBotoes() {
-      document.addEventListener("click", (e) => {
-        //arrow function permite "segurar" o this sem conflitar com o objeto document
-        const el = e.target;
-        if (el.classList.contains("btn-num")) {
-          this.btnParaDisplay(el.innerText);
-        }
-
-        if (el.classList.contains("btn-clear")) {
-          this.clearDisplay();
-        }
-
-        if (el.classList.contains("btn-del")) {
-          this.apagaUm();
-        }
-
-        if (el.classList.contains("btn-eq")) {
-          this.realizaConta();
-        }
-
-        this.display.focus();
-      });
-    },
-
-    btnParaDisplay(valor) {
-      this.display.value += valor;
-    },
+      this.display.value = String(conta);
+    } catch (e) {
+      alert("Conta inválida");
+      return;
+    }
   };
+
+  this.clearDisplay = function () {
+    this.display.value = "";
+  };
+
+  this.apagaUm = function () {
+    this.display.value = this.display.value.slice(0, -1);
+  };
+
+  (this.cliqueBotoes = function () {
+    document.addEventListener("click", (e) => {
+      //arrow function permite "segurar" o this sem conflitar com o objeto document
+      const el = e.target;
+      if (el.classList.contains("btn-num")) {
+        this.btnParaDisplay(el.innerText);
+      }
+
+      if (el.classList.contains("btn-clear")) {
+        this.clearDisplay();
+      }
+
+      if (el.classList.contains("btn-del")) {
+        this.apagaUm();
+      }
+
+      if (el.classList.contains("btn-eq")) {
+        this.realizaConta();
+      }
+
+      this.display.focus();
+    });
+  }),
+    (this.btnParaDisplay = function (valor) {
+      this.display.value += valor;
+    });
 }
-const calculadora = criaCalculadora();
+
+const calculadora = new Calculadora();
 calculadora.inicia();
