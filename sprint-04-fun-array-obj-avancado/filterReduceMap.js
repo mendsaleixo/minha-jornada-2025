5d26d0b84920a7dc2ab5a26aa67b039b8eb89c39
“MendsAleixo” <mendelson.aleixo@gmail.com>
2025-06-19 06:37:11 -0300

    Refactor: melhora o código do  exercício com filter, map e reduce.

--- sprint-04-fun-array-obj-avancado/filterReduceMap.js
+++ sprint-04-fun-array-obj-avancado/filterReduceMap.js
@@ -16,9 +16,8 @@ const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
 // }, 0);
 // console.log(somaTudo);
 
-const somaDobroPares = numeros.filter((valor) => {
-  if (valor % 2 === 0) return valor;
-}).map((valor) => valor * 2).reduce((acumulador, valor) => {
-  return (acumulador += valor);
-}, 0);
+const somaDobroPares = numeros
+.filter((valor) => valor % 2 === 0)
+.map((valor) => valor * 2)
+.reduce((acumulador, valor) => acumulador += valor);
 console.log(somaDobroPares);
