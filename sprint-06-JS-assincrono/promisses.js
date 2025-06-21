4537e9e3e48fc8d6013625194381d1f5b59ed56f
“MendsAleixo” <mendelson.aleixo@gmail.com>
2025-06-21 08:00:38 -0300

    Feat: Praticando  metodos Promise.all e Promise.race.

--- sprint-06-JS-assincrono/promisses.js
+++ sprint-06-JS-assincrono/promisses.js
@@ -36,33 +36,72 @@ Problema de aninhar várias funções (árvore de natal)
 */
 
 //Usando Promisse
+// function esperaAi(msg, tempo) {
+//   return new Promise((resolve, reject) => {
+//     if (typeof msg !== "string") reject("BAD VALUE");
+
+//     setTimeout(() => {
+//       resolve(msg);
+//     }, tempo);
+//   });
+// }
+
+// esperaAi("Conectando com BD", rand(1, 3))
+//   .then((resposta) => {
+//     console.log(resposta);
+//     return esperaAi("Buscando dados da BASE", rand(1, 3));
+//   })
+//   .then((resposta) => {
+//     console.log(resposta);
+//     return esperaAi("Tratando os dados", rand(1, 3));
+//   })
+//   .then((resposta) => {
+//     console.log(resposta);
+//   })
+//   .then(() => {
+//     console.log("Exibindo dados na tela");
+//   })
+//   .catch((e) => {
+//     console.log("ERRO: ", e);
+//   });
+
+// console.log("Exibido antes das promises - Ex: Tela de loading");
+
+//Métodos úteis
 function esperaAi(msg, tempo) {
   return new Promise((resolve, reject) => {
-    if (typeof msg !== "string") reject("BAD VALUE");
+    if (typeof msg !== "string") reject(false);
 
     setTimeout(() => {
-      resolve(msg);
+      resolve(msg.toUpperCase() + " - Passei pela promise");
     }, tempo);
   });
 }
 
-esperaAi("Conectando com BD", rand(1, 3))
-  .then((resposta) => {
-    console.log(resposta);
-    return esperaAi("Buscando dados da BASE", rand(1, 3));
+//Promise.all, Promisse.race, Promise.resolve, Promise.reject
+const promises = [
+  "Primeiro valor (sem promise)",
+  esperaAi("Promise 1", 3000),
+  esperaAi("Promise 2", 500),
+  esperaAi("Promise 3", 1000),
+  //esperaAi("2000", 1000),
+  "Outro valor sem promise",
+];
+
+//Vai trazer todas as promises na ordem
+Promise.all(promises)
+  .then(function (valor) {
+    console.log(valor);
   })
-  .then((resposta) => {
-    console.log(resposta);
-    return esperaAi("Tratando os dados", rand(1, 3));
-  })
-  .then((resposta) => {
-    console.log(resposta);
-  })
-  .then(() => {
-    console.log("Exibindo dados na tela");
-  })
-  .catch((e) => {
-    console.log("ERRO: ", e);
+  .catch(function (erro) {
+    console.log(erro);
   });
 
-console.log("Exibido antes das promises - Ex: Tela de loading");
+//Entrega o primeiro valor que resolver (principalmente sem promise)
+Promise.race(promises)
+  .then(function (valor) {
+    console.log(valor);
+  })
+  .catch(function (erro) {
+    console.log(erro);
+  });
