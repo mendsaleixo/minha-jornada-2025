af99f554f09783ecb0c4c14bbdb2d09955b90227
“MendsAleixo” <mendelson.aleixo@gmail.com>
2025-06-29 12:58:37 -0300

    Feat: Estilizando a lista.

--- sprint-07-react/lista-tarefas/src/components/Main.js
+++ sprint-07-react/lista-tarefas/src/components/Main.js
@@ -3,10 +3,14 @@ import React, { Component } from "react";
 //Form
 import { FaPlus } from "react-icons/fa";
 
+//Tarefas
+import { FaEdit, FaWindowClose } from "react-icons/fa";
+
 import "./Main.css";
 export default class Main extends Component {
   state = {
     novaTarefa: "",
+    tarefas: ["Fazer café", "Beber água", "Estudar"],
   };
 
   handleChange = (e) => {
@@ -17,7 +21,7 @@ export default class Main extends Component {
 
   render() {
     // eslint-disable-next-line no-unused-vars
-    const { novaTarefa } = this.state;
+    const { novaTarefa, tarefas } = this.state;
 
     return (
       <div className="main">
@@ -28,6 +32,17 @@ export default class Main extends Component {
             <FaPlus />
           </button>
         </form>
+        <ul className="tarefas">
+          {tarefas.map((tarefa) => (
+            <li key={tarefa}>
+              {tarefa}
+              <div>
+                <FaEdit className="edit"/>
+                <FaWindowClose className="delete"/>
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
