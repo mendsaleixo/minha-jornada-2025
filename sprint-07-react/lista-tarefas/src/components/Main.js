4217f7331cb8d4f3e099b39f86e6cbc6e22b99e6
“MendsAleixo” <mendelson.aleixo@gmail.com>
2025-06-30 06:50:04 -0300

    Feat: Incluindo tarefas.

--- sprint-07-react/lista-tarefas/src/components/Main.js
+++ sprint-07-react/lista-tarefas/src/components/Main.js
@@ -10,7 +10,21 @@ import "./Main.css";
 export default class Main extends Component {
   state = {
     novaTarefa: "",
-    tarefas: ["Fazer café", "Beber água", "Estudar"],
+    tarefas: [],
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { tarefas } = this.state;
+    let { novaTarefa } = this.state;
+    novaTarefa = novaTarefa.trim();
+
+    if (tarefas.indexOf(novaTarefa) !== -1) return;
+
+    const novasTarefas = [...tarefas];
+    this.setState({
+      tarefas: [...novasTarefas, novaTarefa],
+    });
   };
 
   handleChange = (e) => {
@@ -26,7 +40,7 @@ export default class Main extends Component {
     return (
       <div className="main">
         <h1>Lista de tarefas</h1>
-        <form action="#" className="form">
+        <form onSubmit={this.handleSubmit} action="#" className="form">
           <input onChange={this.handleChange} type="text" value={novaTarefa} />
           <button type="submit">
             <FaPlus />
@@ -36,10 +50,10 @@ export default class Main extends Component {
           {tarefas.map((tarefa) => (
             <li key={tarefa}>
               {tarefa}
-              <div>
-                <FaEdit className="edit"/>
-                <FaWindowClose className="delete"/>
-              </div>
+              <span>
+                <FaEdit className="edit" />
+                <FaWindowClose className="delete" />
+              </span>
             </li>
           ))}
         </ul>
